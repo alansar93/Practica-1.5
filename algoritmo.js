4f835a81d31cecb6517c66bb91c133a5f6ca0478
Alan Sarchman <alansarchman@live.com.ar>
2023-03-31 09:41:09 -0300

    actualizado

---
+++ algoritmo.js
@@ -0,0 +1,15 @@
+document.querySelector("body").innerHTML = `
+<h1>CAMBIO DE ACEITE PARA LA MOTO</h1>
+    <img src="cambio-aceite-5.jpeg" alt="" srcset="">
+    <ol>
+     <li> Conseguir repositorio para aceite, llave 12, embudo y aceite nuevo</li>
+     <li>Utilizar llave 12 para desajustar la tuerca inferior para que salga el aceite</li>
+     <li>Poner el repositorio debajo de la salida de aceite</li>
+     <li>Dejar salir todo el aceite de la moto hasta que no salga nada</li>
+     <li>Tapar con la tuerca nuevamente donde salia el aceite</li>
+     <li>Desajustar tapa donde ingresa el aceite nuevo </li>
+     <li>Introducir el embudo donde desajustamos en el paso anterior</li>
+     <li>Verter el aceite nuevo en el embudo para que vaya entrando a la moto </li>
+     <li>Al finalizar el nuevo aceite sacar el embudo</li>
+     <li>volver ajustar la tapa donde introdujimos el aceite</li>
+    </ol>`
